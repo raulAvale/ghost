6990aa21f522e5f8f7e83345b0b0262799668595
raulAvale <94195203+raulAvale@users.noreply.github.com>
2021-12-27 16:10:06 -0300

    Add files via upload

---
+++ Ghost_Tower_PRO_C19_2021_11_22_20_05_56/sketch.js
@@ -0,0 +1,31 @@
+var imagemDaTorre, torre;
+var imagemDaPorta, porta, grupoDePortas;
+var imagemDeEscalador, escalador, grupoDeEscaladores;
+var fantasma, imagemDoFantasma;
+var grupoDeBlocoInvisivel, blocoInvisivel;
+var estadoJogo = "JOGAR"
+
+function preload(){
+  imagemDaTorre = loadImage("tower.png");
+  imagemDaPorta = loadImage("door.png");
+  imagemDeEscalador = loadImage("climber.png");
+  imagemDoFantasma = loadImage("ghost-standing.png");
+  somAssustador = loadSound("spooky.wav");
+}
+
+function setup(){
+  createCanvas(600,600);
+  
+  torre = createSprite(300,300);
+  torre.addImage("tower",imagemDaTorre);
+  torre.velocityY = 1;
+  }
+
+
+function draw(){
+  background(200);
+  if(torre.y > 400){
+      torre.y = 30
+    }
+  
+}
